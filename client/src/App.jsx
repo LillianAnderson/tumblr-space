a7a3c4e6aa6c749ee6f7c1bd8476cdc652d75990
Lillian <lilliananderson@ucla.edu>
2018-02-01 19:45:52 -0800

    sets up form for blog name and tags

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,43 @@ import axios from 'axios';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      blogName: '',
+      tag: '',
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleBlogNameChange = this.handleBlogNameChange.bind(this);
+    this.handleTagChange = this.handleTagChange.bind(this);
+  }
+
+  handleClick() {
+    console.log(this.state.blogName, this.state.tag)
+    if (this.state.blogName && !this.state.tag) {
+      //do something
+    } else if (this.state.blogName && this.state.tag) {
+      //do something
+    } else if (!this.state.blogName && this.state.tag) {
+      //do something
+    } 
+  }
+
+  handleBlogNameChange(e) {
+    this.setState({blogName: e.target.value})
+  }
+
+  handleTagChange(e) {
+    this.setState({tag: e.target.value})
   }
 
   render() {
     return (
-      <div>
-        hello world
+      <div className='search'>
+          <form>
+            <input id='blog_name' value={this.state.blogName} onChange={this.handleBlogNameChange}/>
+            <input id='tag' value={this.state.tag} onChange={this.handleTagChange}/>
+          </form>
+          <button onClick={this.handleClick}>SEARCH</button>
       </div>
     )
   }
