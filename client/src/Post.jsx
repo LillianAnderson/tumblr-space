85957643afba9a0dd785f4d4d4b6740870c8630f
Lillian <lilliananderson@ucla.edu>
2018-02-01 20:03:23 -0800

    sets uppost structure for type photo

--- client/src/Post.jsx
+++ client/src/Post.jsx
@@ -6,10 +6,22 @@ const Post = ({post}) => {
   return (
     <div className='post'>
       <p>{post.blog_name}</p>
-      
+
       {/* if post is text */}
       {post.type === 'text' &&
         <div dangerouslySetInnerHTML={{__html: post.body}}></div>
+      }
+
+      {/* if post is photo */}
+      {post.type === 'photo' &&
+        <div>
+          <div>
+            {post.photos.map((photo) => {
+              return <img src={photo.original_size.url} key={post.id}/>;
+            })}
+          </div>
+          <div dangerouslySetInnerHTML={{__html: post.caption}}></div>
+        </div>
       }
     </div>
   )
