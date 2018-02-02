659e3c189e22f76a387dfffebaff3c4846536b0a
Lillian <lilliananderson@ucla.edu>
2018-02-01 19:32:57 -0800

    sets up webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+const path = require('path');
+
+const webpackConfig = {
+  entry: path.resolve(__dirname, './client/src/index.js'),
+  output: {
+    path: path.resolve(__dirname, './client/dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
+
+webpackConfig.module.loaders.push({
+  test: /\.js[x]?$/,
+  exclude: /node_modules/,
+  loader: 'babel-loader',
+  options: { 
+    presets: ['es2015', 'react']
+  },
+});
+
+module.exports = webpackConfig;
